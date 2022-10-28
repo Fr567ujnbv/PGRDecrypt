b838d7a668979a2338a9ef654a4aa45150877c8e
Lamp <fr567ujnbv@hotmail.com>
2022-10-27 22:43:06 -0400

    Add project files.

---
+++ FileReader.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace PGRDecrypt
+{
+	internal class FileReader : BinaryReader
+	{
+		internal FileReader(Stream input, EndianType endianType) : base(input, Encoding.UTF8, true)
+		{
+			Stream = input;
+			EndianType = endianType;
+		}
+
+		internal bool TryReadStringNullTerm(out string result) => TryReadStringNullTerm(m_buffer.Length, out result);
+
+		internal bool TryReadStringNullTerm(int length, out string result)
+		{
+			length = (int)Math.Min(Stream.Length, length);
+			for (int i = 0; i < length; i++)
+			{
+				byte bt = ReadByte();
+				if (bt == 0)
+				{
+					result = Encoding.UTF8.GetString(m_buffer, 0, i);
+					return true;
+				}
+				m_buffer[i] = bt;
+			}
+			result = null;
+			return false;
+		}
+
+		public Stream Stream { get; }
+		public EndianType EndianType { get; }
+
+
+		protected const int BufferSize = 4096;
+
+		private readonly byte[] m_buffer = new byte[BufferSize];
+
+		internal bool TryReadInt32(out int result)
+		{
+			int offset = 0;
+			int count = 4;
+			while (count > 0)
+			{
+				int read = Read(m_buffer, offset, count);
+				if (read == 0)
+				{
+					result = 0;
+					return false;
+					//throw new Exception($"End of stream. Read {offset}, expected {count} bytes");
+				}
+				offset += read;
+				count -= read;
+			}
+			result = EndianType == EndianType.LittleEndian ?
+				(m_buffer[0] << 0) | (m_buffer[1] << 8) | (m_buffer[2] << 16) | (m_buffer[3] << 24) :
+				(m_buffer[3] << 0) | (m_buffer[2] << 8) | (m_buffer[1] << 16) | (m_buffer[0] << 24);
+			return true;
+		}
+
+		internal bool TryReadUInt64(out ulong result)
+		{
+			int offset = 0;
+			int count = 8;
+			while (count > 0)
+			{
+				int read = Read(m_buffer, offset, count);
+				if (read == 0)
+				{
+					result = 0;
+					return false;
+					//throw new Exception($"End of stream. Read {offset}, expected {count} bytes");
+				}
+				offset += read;
+				count -= read;
+			}
+			if (EndianType == EndianType.LittleEndian)
+			{
+				uint value1 = unchecked((uint)((m_buffer[0] << 0) | (m_buffer[1] << 8) | (m_buffer[2] << 16) | (m_buffer[3] << 24)));
+				uint value2 = unchecked((uint)((m_buffer[4] << 0) | (m_buffer[5] << 8) | (m_buffer[6] << 16) | (m_buffer[7] << 24)));
+				result = ((ulong)value1 << 0) | ((ulong)value2 << 32);
+			}
+			else
+			{
+				uint value1 = unchecked((uint)((m_buffer[7] << 0) | (m_buffer[6] << 8) | (m_buffer[5] << 16) | (m_buffer[4] << 24)));
+				uint value2 = unchecked((uint)((m_buffer[3] << 0) | (m_buffer[2] << 8) | (m_buffer[1] << 16) | (m_buffer[0] << 24)));
+				result = ((ulong)value1 << 0) | ((ulong)value2 << 32);
+			}
+			return true;
+		}
+
+		internal bool TryReadHash128(out Guid guid)
+		{
+			if (TryReadUInt32(out uint Data0) && TryReadUInt32(out uint Data1) && TryReadUInt32(out uint Data2) && TryReadUInt32(out uint Data3))
+			{
+				StringBuilder sb = new StringBuilder(32, 32);
+				try
+				{
+					sb.Append(Data0.ToString("x8"));
+					sb.Append(Data1.ToString("x8"));
+					sb.Append(Data2.ToString("x8"));
+					sb.Append(Data3.ToString("x8"));
+					guid = new Guid(sb.ToString());
+					if (!guid.Equals(new Guid()))
+					{
+#warning TODO: Need to test if bit order is correct
+					}
+					return true;
+				}
+				finally
+				{
+					sb.Clear();
+				}
+			}
+			guid = Guid.Empty;
+			return false;
+			throw new NotImplementedException();
+		}
+
+		internal bool TryReadUInt16(out ushort result)
+		{
+			int offset = 0;
+			int count = 2;
+			while (count > 0)
+			{
+				int read = Read(m_buffer, offset, count);
+				if (read == 0)
+				{
+					result = 0;
+					return false;
+					//throw new Exception($"End of stream. Read {offset}, expected {count} bytes");
+				}
+				offset += read;
+				count -= read;
+			}
+			result = EndianType == EndianType.LittleEndian ?
+				unchecked((ushort)((m_buffer[0] << 0) | (m_buffer[1] << 8))) :
+				unchecked((ushort)((m_buffer[1] << 0) | (m_buffer[0] << 8)));
+			return true;
+		}
+
+		internal bool TryReadUInt32(out uint result)
+		{
+			int offset = 0;
+			int count = 4;
+			while (count > 0)
+			{
+				int read = Read(m_buffer, offset, count);
+				if (read == 0)
+				{
+					result = 0;
+					return false;
+					//throw new Exception($"End of stream. Read {offset}, expected {count} bytes");
+				}
+				offset += read;
+				count -= read;
+			}
+			result = EndianType == EndianType.LittleEndian ?
+				unchecked((uint)((m_buffer[0] << 0) | (m_buffer[1] << 8) | (m_buffer[2] << 16) | (m_buffer[3] << 24))) :
+				unchecked((uint)((m_buffer[3] << 0) | (m_buffer[2] << 8) | (m_buffer[1] << 16) | (m_buffer[0] << 24)));
+			return true;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PGRDecrypt
+{
+	class Program
+	{
+		static void OutputUsage(string title)
+		{
+			Console.Write(
+$@"{title}
+
+Windows Explorer Drag-n-Drop Usage:
+  Simply drop an encrypted PGR file into {AppDomain.CurrentDomain.FriendlyName} to create a new decrypted PGR file.
+
+Command Line Usage:
+  {AppDomain.CurrentDomain.FriendlyName} [-s] <source> [-d <destination>] [-k <decryptionkey> [-b64]] [-o]
+
+  -s source               Path of encrypted PGR file to load.
+  -d destination          Path of decrypted PGR file to save.
+                            Default = {{source}}.decrypted
+  -k keyfile              Key file to decrypt with.
+                             Default = {AppDomain.CurrentDomain.BaseDirectory}{defaultKeyFilename}
+
+Press any key to exit . . ."
+			);
+
+			Console.ReadKey();
+		}
+
+		const string defaultKeyFilename = "keyfile.txt";
+
+		struct Block
+		{
+			public uint UncompressedSize;
+			public uint CompressedSize;
+			public ushort Flags;
+		}
+
+		struct DirectoryInfo
+		{
+			public ulong Offset;
+			public ulong Size;
+			public uint Flags;
+			public string Path;
+		}
+
+		static void Main(string[] args)
+		{
+			if (args.Length == 0)
+			{
+				OutputUsage("Tool to decrypt PGR files by Lamp");
+				return;
+			}
+			string sArg = null;
+			string dArg = null;
+			string kArg = null;
+			for (int i = 0, nexti = 1; i < args.Length; i = nexti, nexti++)
+			{
+				var arg = args[i];
+				if (arg.Equals("-s") && args.Length > nexti)
+				{
+					sArg = args[nexti++];
+				}
+				else if (arg.Equals("-d") && args.Length > nexti)
+				{
+					dArg = args[nexti++];
+				}
+				else if (arg.Equals("-k") && args.Length > nexti)
+				{
+					kArg = args[nexti++];
+				}
+				else
+				{
+					if (!string.IsNullOrEmpty(sArg))
+					{
+						OutputUsage("Multiple source files are not supported!");
+						return;
+					}
+					sArg = args[i];
+				}
+			}
+
+			if (string.IsNullOrEmpty(sArg))
+			{
+				OutputUsage("Source path not specified!");
+				return;
+			}
+
+			if (!File.Exists(sArg))
+			{
+				OutputUsage($"Source file '{Path.GetFullPath(sArg)}' does not exist!");
+				return;
+			}
+
+			if (string.IsNullOrEmpty(dArg))
+			{
+				dArg = Path.GetFullPath(sArg) + ".decrypted";
+			}
+
+			if (File.Exists(dArg) || Directory.Exists(dArg))
+			{
+				OutputUsage($"Destination '{Path.GetFullPath(dArg)}' already exist!");
+				return;
+			}
+
+			string[] keys;
+			string keyfile;
+			if (string.IsNullOrEmpty(kArg))
+			{
+				keyfile = AppDomain.CurrentDomain.BaseDirectory + defaultKeyFilename;
+				if (!File.Exists(keyfile))
+				{
+					File.WriteAllText(keyfile, "kurokurokurokuro\r\ny5XPvqLOrCokWRIa");
+					/*OutputUsage($"{keyfile} not found! Please use -k switch!");
+					return;*/
+				}
+			}
+			else
+			{
+				keyfile = kArg;
+				if (!File.Exists(keyfile))
+				{
+					OutputUsage($"{keyfile} not found!");
+					return;
+				}
+			}
+			try
+			{
+				keys = File.ReadAllLines(keyfile);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(
+$@"An error occurred trying to read {keyfile}:
+
+{e.Message}
+
+{e.StackTrace}"
+);
+				Console.ReadKey();
+				return;
+			}
+
+#if !DEBUG
+			try
+#endif
+			{
+				using (Stream file = File.OpenRead(sArg))
+				{
+					using (FileReader reader = new FileReader(file, EndianType.BigEndian))
+					{
+						if (!reader.TryReadStringNullTerm(out string type) || type != "UnityFS") // Type
+						{
+							throw new NotSupportedException("File is not UnityFS");
+						}
+						if (!reader.TryReadInt32(out int _) ||                                   // Version
+							!reader.TryReadStringNullTerm(out string _) ||                       // UnityWebBundleVersion
+							!reader.TryReadStringNullTerm(out string _))                         // UnityWebMinimumRevision
+						{
+							throw new InvalidDataException("Cannot read file header");
+						}
+						long bundleSizePtr = file.Position;
+						if (!reader.TryReadUInt64(out ulong bundleSize) ||                       // BundleSize **** Need to update too after stripping PGR header (-= 0x46)
+							!reader.TryReadInt32(out int metadataSize) ||                        // MetadataSize
+							metadataSize == 0 ||
+							!reader.TryReadInt32(out int uncompressedMetadataSize) ||            // UncompressedMetadataSize
+							uncompressedMetadataSize < 24 ||
+							!reader.TryReadInt32(out int flags))                                 // Flags **** Need to update too after stripping PGR header (&= 0xFFFFFDFF)
+						{
+							throw new InvalidDataException("Cannot read file header");
+						}
+						if ((flags & 0x200) == 0)
+						{
+							throw new NotSupportedException("File is not encrypted");
+						}
+						bool goodkey = false;
+						PGR pgr = null;                                                 // **** Need to strip PGR header
+						foreach (string key in keys)
+						{
+							PGR.UpdateKey(key);
+							try
+							{
+								pgr = new PGR(reader);
+								goodkey = true;
+								break;
+							}
+							catch (Exception ex)
+							{
+								if (ex.Message == "Invalid Signature !!")
+								{
+									reader.Stream.Seek(-0x46, SeekOrigin.Current);
+								}
+							}
+						}
+						if (!goodkey)
+						{
+							throw new Exception("Key file provided is invalid");
+						}
+						Console.WriteLine("Reading metadata...");
+						if ((flags & 0x00000080) != 0)
+						{
+							long metaposition = (long)bundleSize - metadataSize;
+							if (metaposition < 0 || metaposition > file.Length)
+							{
+								throw new DataMisalignedException("Metadata offset is out of bounds");
+							}
+							throw new NotImplementedException($"Unsupported metadata position");
+							file.Position = metaposition;
+						}
+						if ((flags & 0x0000003e) != 2)
+						{
+							throw new NotImplementedException($"Compresstion type '0x{flags & 0x0000003f:X2}' is not supported");
+						}
+
+						List<Block> blocks = new List<Block>();
+						List<DirectoryInfo> directories = new List<DirectoryInfo>();
+
+						using (MemoryStream uncompressedMetadata = new MemoryStream(new byte[uncompressedMetadataSize]))
+						{
+							using (Lz4DecodeStream decodeStream = new Lz4DecodeStream(file, metadataSize))
+							{
+								decodeStream.ReadBuffer(uncompressedMetadata, uncompressedMetadataSize);
+								uncompressedMetadata.Position = 0;
+								using (FileReader metaReader = new FileReader(uncompressedMetadata, EndianType.BigEndian))
+								{
+									if (!metaReader.TryReadHash128(out Guid _))
+									{
+										throw new InvalidDataException("Cannot read file metadata");
+									}
+
+									if (!metaReader.TryReadUInt32(out uint blockcount) || blockcount * 10 + 20 > uncompressedMetadataSize)
+									{
+										throw new InsufficientMemoryException($"Block count of {blockcount} is too large for metadata size");
+									}
+									string plural = (blockcount == 1) ? "" : "s";
+									Console.WriteLine($"{blockcount} block{plural} found:");
+									for (int i = 0; i < blockcount; i++)
+									{
+										metaReader.TryReadUInt32(out uint uncompressedsize);
+										metaReader.TryReadUInt32(out uint compressedsize);
+										metaReader.TryReadUInt16(out ushort blockflags);
+										Console.WriteLine($"  {i}: ({uncompressedsize}) {compressedsize} 0x{blockflags:X4}");
+										blocks.Add(new Block { UncompressedSize = uncompressedsize, CompressedSize = compressedsize, Flags = blockflags });
+									}
+
+									if (!metaReader.TryReadUInt32(out uint directorycount) || directorycount * 10 + blockcount * 10 + 20 > uncompressedMetadataSize)
+									{
+										throw new InsufficientMemoryException($"Directory count of {directorycount} is too large for metadata size");
+									}
+									plural = (directorycount == 1) ? "y" : "ies";
+									Console.WriteLine($"{directorycount} director{plural} found:");
+									for (int i = 0; i < directorycount; i++)
+									{
+										metaReader.TryReadUInt64(out ulong offset);
+										metaReader.TryReadUInt64(out ulong size);
+										metaReader.TryReadUInt32(out uint directoryflags);
+#warning TODO: Limit string parsing to remaining array size
+										metaReader.TryReadStringNullTerm(out string path);
+										Console.WriteLine($"  {i}: @{offset:X} {size} {directoryflags} \"{path}\"");
+										directories.Add(new DirectoryInfo { Offset = offset, Size = size, Flags = directoryflags, Path = path });
+									}
+								}
+							}
+						}
+
+						using (FileStream writer = File.OpenWrite(dArg))
+						{
+							long blockPtr = file.Position;
+							file.Position = 0;
+
+							// Write header
+							for (int i = 0; i < (int)blockPtr - metadataSize - 0x46; i++)
+							{
+								writer.WriteByte(reader.ReadByte());
+							}
+
+							// Patch out encyption flag
+							// (even though it is ignored by most 3rd party tools)
+							writer.Position -= 2;
+							writer.WriteByte((byte)(flags >> 8 & 0xFD));
+							writer.WriteByte((byte)(flags));
+
+							// Skip encryption header
+							file.Position = blockPtr - metadataSize;
+
+							// Write metadata
+							for (int i = 0; i < metadataSize; i++)
+							{
+								writer.WriteByte(reader.ReadByte());
+#warning TODO: Patch out encrypted block flag in metadata
+							}
+
+							// Patch bundles size
+							writer.Position = bundleSizePtr;
+							bundleSize -= 0x46;
+							writer.WriteByte((byte)(bundleSize >> 56));
+							writer.WriteByte((byte)(bundleSize >> 48));
+							writer.WriteByte((byte)(bundleSize >> 40));
+							writer.WriteByte((byte)(bundleSize >> 32));
+							writer.WriteByte((byte)(bundleSize >> 24));
+							writer.WriteByte((byte)(bundleSize >> 16));
+							writer.WriteByte((byte)(bundleSize >> 8));
+							writer.WriteByte((byte)(bundleSize));
+							writer.Position = blockPtr - 0x46;
+
+							for (int i = 0; i < blocks.Count; i++)
+							{
+								Block block = blocks[i];
+								if ((block.Flags & 0x0000003e) != 2)
+								{
+									throw new NotImplementedException($"Compresstion type '0x{block.Flags & 0x0000003f:X2}' is not supported");
+								}
+								int compressedSize = (int)block.CompressedSize;
+								byte[] compressedBytes = new byte[compressedSize];
+								reader.Read(compressedBytes, 0, compressedSize);
+								if ((block.Flags & 0x100) != 0)
+								{
+									pgr.DecryptBlock(compressedBytes, compressedSize, i);
+								}
+
+								Console.WriteLine($"Decrypted block {i} of {blocks.Count}");
+								writer.Write(compressedBytes, 0, compressedSize);
+							}
+						}
+					}
+				}
+			}
+#if !DEBUG
+			catch (Exception e)
+			{
+				Console.WriteLine(
+$@"An error occurred trying to read {sArg}
+{e.StackTrace}
+
+{e.Message}"
+);
+				Console.ReadKey();
+				return;
+			}
+#endif
+		}
+	}
+}
